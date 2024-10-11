b0de3ca08062bc48434a4dbfbc5b7661121701c6
DESKTOP-M81N00P\Ketto <caijack1403@gmail.com>
2024-10-11 02:26:51 +0200

    add contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {CdkCopyToClipboard, Clipboard} from '@angular/cdk/clipboard';
+import {TooltipModule} from "primeng/tooltip";
+import {InputTextModule} from "primeng/inputtext";
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    CdkCopyToClipboard,
+    TooltipModule,
+    InputTextModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  email: string = "caijack1403@gmail.com";
+  location: string = "Paris, France";
+  github: string = "https://github.com/Jack-CAI2001";
+  linkedIn: string = "https://www.linkedin.com/in/jack-cai-30221b185/";
 }
