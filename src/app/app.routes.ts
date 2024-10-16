2a2fefdba41014f8647e28f9beedd065450d73cf
DESKTOP-M81N00P\Ketto <caijack1403@gmail.com>
2024-10-16 19:01:38 +0200

    add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import {ContactComponent} from "./components/contact/contact.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'experiences', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '/home', pathMatch: 'full' },
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+import Typewriter from 't-writer.js';
+import {AsyncPipe, NgForOf, NgOptimizedImage} from "@angular/common";
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgOptimizedImage,
+    Button,
+    NgForOf
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  protected downloadCV() {
+    const pdfUrl = '/CAIJack.pdf'
+    const pdfName = "CV_Jack_CAI.pdf"
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = pdfName;
+    link.click();
+  }
+  @ViewChild('tw') typewriterElement!: ElementRef;
 
+  constructor() {
+    afterNextRender(() => {
+      const target = this.typewriterElement.nativeElement;
+
+      const writer = new Typewriter(target, {
+        typeColor: '#9918EE',
+        cursorColor: '#9918EE',
+        loop: true,
+        typeSpeed: 80,
+        deleteSpeed: 80,
+      })
+
+      writer
+        .strings(
+          1500,
+          "Android Developer",
+          "Fullstack Developer",
+          "Backend Developer"
+        )
+        .start()
+    })
+  }
 }
+
